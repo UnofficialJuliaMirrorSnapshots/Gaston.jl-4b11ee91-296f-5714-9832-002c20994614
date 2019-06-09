9f22f66d441e11a9e22ef30beebeec893fcb504a
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-09 01:44:14 +0000

    Snapshot of branch master taken on 2019-06-08T21:44:12.999-04:00 from "https://github.com/UnofficialJuliaMirror/Gaston.jl-4b11ee91-296f-5714-9832-002c20994614"

--- test/runtests.jl
+++ test/runtests.jl
@@ -208,8 +208,10 @@ end
         plot!(1:10,financial=fin,plotstyle="financebars")
         Gaston.gnuplot_state.gp_error
     end == false
-    # test requires gnuplot 5.2
-    if occursin("5.2",read(`gnuplot --version`, String))
+    # This test is cool, but it fails way too often due to slight
+    # OS, configuration, and gnuplot version differences. Disabled for now.
+    if false
+    #if occursin("5.2",read(`gnuplot --version`, String))
         @test begin
             set(reset=true)
             set(terminal="dumb", size="27,13")
@@ -217,13 +219,13 @@ end
             a=repr("text/plain", plot(1:10))
             a == x
         end == true
-        @test begin
-            set(reset=true)
-            set(terminal="ijulia")
-            a = repr("text/plain", plot(1:10))
-            a[1:35] == "<?xml version=\"1.0\" encoding=\"utf-8"
-        end == true
     end
+    @test begin
+        set(reset=true)
+        set(terminal="ijulia")
+        a = repr("text/plain", plot(1:10))
+        a[1:35] == "<?xml version=\"1.0\" encoding=\"utf-8"
+    end == true
     # build a multiple-plot figure manually
     closeall()
     set(terminal="null")
@@ -241,7 +243,7 @@ end
         theo_curve = Gaston.Curve(x2,theo_pdf,theo_cconf)
         figure(1)
         Gaston.push_figure!(1,ac,exp_curve,theo_curve)
-        figure(1)  # plot the figure
+        figure(1,redraw=false)
     end == 1
 end
 
@@ -356,7 +358,7 @@ end
     closeall()
     set(reset=true)
     set(terminal="null")
-    set(print_outputfile="$(tempdir())/gastontest")
+    set(print_outputfile=tempname())
     plot(1:10)
     @test begin
         printfigure()
@@ -383,13 +385,11 @@ end
         printfigure(term="svg")
         Gaston.gnuplot_state.gp_error
     end == false
-    if !Sys.iswindows()
-        @test begin
-            set(print_size="640,480")
-            printfigure(term="gif")
-            Gaston.gnuplot_state.gp_error
-        end == false
-    end
+    @test begin
+        set(print_size="640,480")
+        printfigure(term="gif")
+        Gaston.gnuplot_state.gp_error
+    end == false
 end
 
 @testset "Linestyle tests" begin
@@ -430,41 +430,44 @@ end
     # but there seems to be no way to create them in Julia, so we pipe
     # data through an external process. The advantage of this approach
     # is that it corresponds to how Gaston communicates with gnuplot.
-    pin = Pipe()
-    pout = Pipe()
-    proc = try
-        run(pipeline(`cat`, stdin = pin, stdout = pout),
-            wait = false)
-    catch err
-        @warn "Skipping async_reader tests"
-        return
+    # This testset is only enabled on Unix-like systems.
+    if Sys.isunix()
+        pin = Pipe()
+        pout = Pipe()
+        proc = try
+            run(pipeline(`cat`, stdin = pin, stdout = pout),
+                wait = false)
+        catch err
+            @warn "Skipping async_reader tests"
+            return
+        end
+        close(pout.in)
+        close(pin.out)
+
+        @test begin
+            ch = Gaston.async_reader(pout, 1)
+            write(pin, "GastonBegin\ncontent\nGastonDone\n")
+            take!(ch)
+        end == "content\n"
+
+        @test begin
+            ch = Gaston.async_reader(pout, 0.001)
+            write(pin, "GastonBegin\nmissing end pointtype\n")
+            take!(ch)
+        end === :timeout
+
+        @test begin
+            ch = Gaston.async_reader(pout, 1)
+            write(pin, "no begin pointtype\nGastonDone\n")
+            take!(ch)
+        end == "no begin pointtype\n"
+
+        @test begin
+            ch = Gaston.async_reader(pout, 1)
+            kill(proc)
+            take!(ch)
+        end === :eof
     end
-    close(pout.in)
-    close(pin.out)
-
-    @test begin
-        ch = Gaston.async_reader(pout, 1)
-        write(pin, "GastonBegin\ncontent\nGastonDone\n")
-        take!(ch)
-    end == "content\n"
-
-    @test begin
-        ch = Gaston.async_reader(pout, 0.001)
-        write(pin, "GastonBegin\nmissing end pointtype\n")
-        take!(ch)
-    end === :timeout
-
-    @test begin
-        ch = Gaston.async_reader(pout, 1)
-        write(pin, "no begin pointtype\nGastonDone\n")
-        take!(ch)
-    end == "no begin pointtype\n"
-
-    @test begin
-        ch = Gaston.async_reader(pout, 1)
-        kill(proc)
-        take!(ch)
-    end === :eof
 end
 
 @testset "Tests that should fail" begin
@@ -481,6 +484,7 @@ end
     @test_throws MethodError closefigure(1:2)
     # plot
     @test_throws DimensionMismatch plot(0:10,0:11)
+    @test_throws DimensionMismatch surf([1,2],[3,4],[5,6])
     @test_throws TypeError plot(0:10,legend=0)
     @test_throws DomainError plot(0:10,plotstyle="invalid")
     @test_throws DomainError plot(0:10,pointtype="invalid")
